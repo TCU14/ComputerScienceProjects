5c196574dd91359f777788b5f603be938038ea09
Everett Westman <38740385+TCU14@users.noreply.github.com>
2020-02-11 09:44:52 -0700

    Add files via upload

---
+++ WorstCodeEver.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main {
+    public static void main (final String[] args) {
+        System.out.println("Hello Wonderful World!");
+        System.out.print("What is your name fellow child? ");
+        final Scanner scanner = new Scanner (System.in);
+        final String name = scanner.next();
+        System.out.println(name + " is your name? You should change it.");
+        System.out.println("Listen here ya little shit, what sport do you like");
+       final Scanner SPORTS = new Scanner (System.in);
+        final String sport = SPORTS.next();
+        System.out.println (sport + "? Dude, that sport is tasteless");
+        System.out.println ("What fashion brand do you like");        final Scanner FASHION = new Scanner (System.in);
+        final String fashion = FASHION.next();
+        System.out.println (fashion + "... Sorry not sorry, that brand sucks.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
